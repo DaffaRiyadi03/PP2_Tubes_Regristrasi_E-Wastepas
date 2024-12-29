c70d3afbde232c6a795bd03e7e28bfb9252cb064
DaffaRiyadi03 <daffariyadi100@gmail.com>
2024-12-29 12:23:54 +0700

    Mengerjakan halaman login,register dan dashboard

---
+++ src/main/java/tubes/ewaste/Main.java
@@ -0,0 +1,20 @@
+package tubes.ewaste;
+
+import com.formdev.flatlaf.FlatLightLaf;
+import tubes.ewaste.view.MainFrame;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (Exception e) {
+            System.err.println("Failed to initialize LaF");
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            MainFrame frame = new MainFrame();
+            frame.setVisible(true);
+        });
+    }
+}
